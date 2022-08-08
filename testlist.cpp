d7866a3fb23a6a789751ee0c9c755ef10c02f69e
bstok005 <cs_bstok005@io.infra.cs.odu.edu>
2022-08-08 14:52:47 -0400

    "final commit"

--- testlist.cpp
+++ testlist.cpp
@@ -14,7 +14,7 @@ const char* names[] = {"Adams", "Baker", "Clarke", "Daniels", "Evans",
 		       "Zimmerman"
 };  
 
-void testList(int numberOfItems)
+void testList(int test, int numberOfItems)
 {
   NameList L;
   // Test part 1 - try adding to the front
@@ -47,7 +47,7 @@ int main()
   
   for (int test = 26; test >= 0; --test)
     {
-      testList(numberOfItems);
+      testList(test, numberOfItems);
     }
 
   return 0;
